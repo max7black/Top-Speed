80d2bc049177711e067c5ea503ae3de70181e4d0
Cole Arnold <max7black@gmail.com>
2016-07-17 23:17:11 -0500

    Started adding speeds based on slopes
    Also fixed a bug where character wouldn't rotate on slopes.

---
+++ Assets/Scripts/Managers/SpeedManagers.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpeedManagers : MonoBehaviour {
+    public static float velocity = 0.0f;
+    Text text;
+
+    // Use this for initialization
+    void Start () {
+        velocity = UnityStandardAssets._2D.PlatformerCharacter2D.currentVelocity;
+        text = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        velocity = UnityStandardAssets._2D.PlatformerCharacter2D.currentVelocity;
+        text.text = "Speed: " + velocity;
+    }
+}
